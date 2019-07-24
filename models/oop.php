f2b5ccf9ec4e9e9e353881413a8d5b0d3a1e8f0c
Dirk Kalmbach <dirk.kalmbach@me.com>
2019-07-24 21:28:23 +0800

    initial

---
+++ models/oop.php
@@ -0,0 +1,15 @@
+<?php
+ 
+class MyClass
+{
+    // Class properties and methods go here
+    public $prop1 = "I'm a class property!";
+}
+ 
+$obj = new MyClass;
+ 
+var_dump($obj);
+print("<hr>");
+echo $obj2->prop1;
+ 
+?>
